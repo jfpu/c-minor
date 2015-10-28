430e7e024b0cccab071ab48790c7af452f259b6d
Shuyang Li <shuyang.li.95@gmail.com>
2015-10-27 22:53:20 -0400

    Implement type_print

--- type.c
+++ type.c
@@ -19,5 +19,40 @@ struct type *type_create_array(struct expr *size, struct type *subtype) {
 }
 
 void type_print(struct type *t) {
+    switch (t->kind) {
+        case TYPE_BOOLEAN:
+            printf("boolean");
+            break;
 
+        case TYPE_CHARACTER:
+            printf("char");
+            break;
+
+        case TYPE_INTEGER:
+            printf("integer");
+            break;
+
+        case TYPE_STRING:
+            printf("string");
+            break;
+
+        case TYPE_ARRAY:
+            printf("array [");
+            expr_print(t->size);
+            printf("] ");
+            type_print(t->subtype);
+            break;
+
+        case TYPE_FUNCTION:
+            printf("function ");
+            type_print(t->subtype);
+            printf(" (");
+            param_list_print(t->params);
+            printf(")");
+            break;
+
+        case TYPE_VOID:
+            printf("void");
+            break;
+    }
 }
