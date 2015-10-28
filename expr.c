9432e1ede60c592824517c92f453781f402f7d5f
Shuyang Li <shuyang.li.95@gmail.com>
2015-10-27 22:52:29 -0400

    Implement expr_print

--- expr.c
+++ expr.c
@@ -68,5 +68,184 @@ struct expr *expr_create_incdec(expr_t kind, const char *name) {
 }
 
 void expr_print(struct expr *e) {
+    if (!e) return;
 
+    int i = 0;
+    switch (e->kind) {
+        case EXPR_NAME:
+            printf("%s", e->name);
+            break;
+
+        case EXPR_BOOLEAN:
+            if (e->literal_value) {
+                printf("true");
+            } else {
+                printf("false");
+            }
+            break;
+
+        case EXPR_INTEGER:
+            printf("%d", e->literal_value);
+            break;
+
+        case EXPR_CHARACTER:
+            if (e->literal_value == '\0') {
+                printf("\\0");
+            } else if (e->literal_value == '\n') {
+                printf("\\n");
+            } else {
+                printf("'%c'", e->literal_value);
+            }
+            break;
+
+        case EXPR_STRING:
+            // We need to unescape each individual character
+            i = 0;
+            printf("\"");
+            while (e->string_literal[i]) {
+                if (e->string_literal[i] == '\n') {
+                    printf("\\n");
+                } else {
+                    printf("%c", e->string_literal[i]);
+                }
+                ++i;
+            }
+            printf("\"");
+            break;
+
+        case EXPR_ASSIGN:
+            expr_print(e->left);
+            printf(" = ");
+            expr_print(e->right);
+            break;
+
+        case EXPR_ADD:
+            expr_print(e->left);
+            printf(" + ");
+            expr_print(e->right);
+            break;
+
+        case EXPR_SUB:
+            expr_print(e->left);
+            printf(" - ");
+            expr_print(e->right);
+            break;
+
+        case EXPR_MUL:
+            printf("(");
+            expr_print(e->left);
+            printf(") * (");
+            expr_print(e->right);
+            printf(")");
+            break;
+
+        case EXPR_DIV:
+            printf("(");
+            expr_print(e->left);
+            printf(") / (");
+            expr_print(e->right);
+            printf(")");
+            break;
+
+        case EXPR_EXP:
+            expr_print(e->left);
+            printf(" ^ ");
+            expr_print(e->right);
+            break;
+
+        case EXPR_MOD:
+            printf("(");
+            expr_print(e->left);
+            printf(") %% (");
+            expr_print(e->right);
+            printf(")");
+            break;
+
+        case EXPR_INC:
+            printf("%s++", e->name);
+            break;
+
+        case EXPR_DEC:
+            printf("%s--", e->name);
+            break;
+
+        case EXPR_LAND:
+            printf("(");
+            expr_print(e->left);
+            printf(") && (");
+            expr_print(e->right);
+            printf(")");
+            break;
+
+        case EXPR_LOR:
+            printf("(");
+            expr_print(e->left);
+            printf(") || (");
+            expr_print(e->right);
+            printf(")");
+            break;
+
+        case EXPR_LNOT:
+            printf("!(");
+            expr_print(e->right);
+            printf(")");
+            break;
+
+        case EXPR_LT:
+            expr_print(e->left);
+            printf(" < ");
+            expr_print(e->right);
+            break;
+
+        case EXPR_LE:
+            expr_print(e->left);
+            printf(" <= ");
+            expr_print(e->right);
+            break;
+
+        case EXPR_GT:
+            expr_print(e->left);
+            printf(" > ");
+            expr_print(e->right);
+            break;
+
+        case EXPR_GE:
+            expr_print(e->left);
+            printf(" >= ");
+            expr_print(e->right);
+            break;
+
+        case EXPR_EQ:
+            expr_print(e->left);
+            printf(" == ");
+            expr_print(e->right);
+            break;
+
+        case EXPR_NE:
+            expr_print(e->left);
+            printf(" != ");
+            expr_print(e->right);
+            break;
+
+        case EXPR_FCALL:
+            printf("%s(", e->name);
+            expr_print(e->params);
+            printf(")");
+            break;
+
+        case EXPR_ARRAY_DEREF:
+            printf("%s[", e->name);
+            expr_print(e->params);
+            printf("]");
+            break;
+
+        default:
+            printf("Expression");
+            break;
+    }
+
+    if (e->next) {
+        printf(", ");
+        expr_print(e->next);
+    }
 }
