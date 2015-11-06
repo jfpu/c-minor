8c2931c93eca18e86055434129bda2b85360eed7
Shuyang Li <shuyang.li.95@gmail.com>
2015-11-06 16:17:45 -0500

    Add which field to symbol_create

--- symbol.c
+++ symbol.c
@@ -2,11 +2,12 @@
 #include <string.h> // memset
 #include "symbol.h"
 
-struct symbol *symbol_create(symbol_t kind, struct type *type, char *name) {
+struct symbol *symbol_create(symbol_t kind, int which, struct type *type, char *name) {
     struct symbol *s = (struct symbol *)malloc(sizeof(*s));
     memset(s, 0, sizeof(*s));
 
     s->kind = kind;
+    s->which = which;
     s->type = type;
     s->name = name;
     return s;
