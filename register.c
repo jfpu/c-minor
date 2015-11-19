c9aa4cd8c4ed797b286c113f8f8c5f0a055e5ed7
Shuyang Li <shuyang.li.95@gmail.com>
2015-11-19 13:59:24 -0500

    Remove extra % in register name

--- register.c
+++ register.c
@@ -12,10 +12,10 @@ int register_allocation_table[16] = {
 
 const char *register_name(int r) {
     static const char *register_name_table[16] = {
-        "%%rax", "%%rbx", "%%rcx", "%%rdx",
-        "%%rsi", "%%rdi", "%%rsp", "%%rbp",
-        "%%r8",  "%%r9",  "%%r10", "%%r11",
-        "%%r12", "%%r13", "%%r14", "%%r15"
+        "%rax", "%rbx", "%rcx", "%rdx",
+        "%rsi", "%rdi", "%rsp", "%rbp",
+        "%r8",  "%r9",  "%r10", "%r11",
+        "%r12", "%r13", "%r14", "%r15"
     };
 
     if (r >= 0 && r < 16) {
