8168abf82905bd37bd85a5e326a5e948b522ab1e
Shuyang Li <shuyang.li.95@gmail.com>
2015-11-24 20:46:37 -0500

    Change order of saving variables for decl
    to easily calculate symbol_code

--- decl.c
+++ decl.c
@@ -259,13 +259,6 @@ void decl_codegen_individual(struct decl *d, FILE *file) {
             fprintf(file, "PUSH %%rbp\n");
             fprintf(file, "MOV %%rsp, %%rbp\n");
 
-            // save callee-save registers
-            fprintf(file, "PUSH %%rbx\n");
-            fprintf(file, "PUSH %%r12\n");
-            fprintf(file, "PUSH %%r13\n");
-            fprintf(file, "PUSH %%r14\n");
-            fprintf(file, "PUSH %%r15\n");
-
             // for each parameter, push it on the stack
             if (d->symbol->param_count > 6) {
                 printf("error: functions with over 6 arguments are not supported\n");
@@ -279,6 +272,13 @@ void decl_codegen_individual(struct decl *d, FILE *file) {
             // for the total number of local variables, make room in the stack
             int rsp_move_amount = 8 * d->symbol->local_count;
             fprintf(file, "SUB $%d, %%rsp\n", rsp_move_amount);
+
+            // then save callee-save registers
+            fprintf(file, "PUSH %%rbx\n");
+            fprintf(file, "PUSH %%r12\n");
+            fprintf(file, "PUSH %%r13\n");
+            fprintf(file, "PUSH %%r14\n");
+            fprintf(file, "PUSH %%r15\n");
 
             // then generate code
             stmt_codegen(d->code, file);
