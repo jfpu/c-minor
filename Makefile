0df20dfdcffadf00ef7e3e88d0afb0033c8ae9c1
Shuyang Li <shuyang.li.95@gmail.com>
2015-11-24 23:23:37 -0500

    Add test harness for codegen

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 FLAGS=-Wall -g
 OBJS=lex.yy.o parser.tab.o decl.o expr.o param_list.o stmt.o type.o utility.o symbol.o scope.o hash_table.o register.o
 
-all: cminor
+all: cminor library.o
 
 cminor: main.o $(OBJS)
 	$(CC) $(FLAGS) main.o $(OBJS) -o cminor -lm
@@ -21,7 +21,7 @@ parse parser.tab.c parser.tab.h: parser.y
 clean:
 	rm -f lex.yy.c lex.yy.h parser.tab.c parser.tab.h parser.output *.o *.s *.out cminor
 
-wipeassembly:
-	rm ./test_codegen/*.s
+wipeass:
+	rm -f ./test_compile/*.s ./test_compile/*.out
 
-.PHONY: all clean lex parse wipeassembly
+.PHONY: all clean lex parse wipeass
--- harness.rb
+++ harness.rb
@@ -1,20 +1,30 @@
 #!/usr/bin/env ruby
 
-if ARGV.count != 1
+trans_dict = {
+  "lex" => "scan",
+  "parse" => "print",
+  "typecheck" => "typecheck",
+  "compile" => "compile"
+}
+
+if ARGV.count != 1 or !trans_dict.has_key?(ARGV[0])
   warn "invalid option [lex, parse, typecheck]"
   exit 1
 end
 
-trans_dict = {
-  "lex" => "scan",
-  "parse" => "print",
-  "typecheck" => "typecheck"
-}
+case ARGV[0]
+when "lex", "parse", "typecheck"
+  Dir["test_#{ARGV[0]}/good*.cminor"].each do |file|
+    warn "#{file} test incorrectly failed" unless system("./cminor -#{trans_dict[ARGV[0]]} #{file} >/dev/null 2>/dev/null")
+  end
 
-Dir["test_#{ARGV[0]}/good*.cminor"].each do |file|
-  warn "#{file} test incorrectly failed" unless system("./cminor -#{trans_dict[ARGV[0]]} #{file} >/dev/null 2>/dev/null")
-end
+  Dir["test_#{ARGV[0]}/bad*.cminor"].each do |file|
+    warn "#{file} test incorrectly passed" if system("./cminor -#{trans_dict[ARGV[0]]} #{file} >/dev/null 2>/dev/null")
+  end
 
-Dir["test_#{ARGV[0]}/bad*.cminor"].each do |file|
-  warn "#{file} test incorrectly passed" if system("./cminor -#{trans_dict[ARGV[0]]} #{file} >/dev/null 2>/dev/null")
+when "compile"
+  Dir["test_#{ARGV[0]}/good*.cminor"].each do |file|
+    warn "#{file} test incorrectly failed" unless system("./cminor -#{trans_dict[ARGV[0]]} #{file} #{file}.s >/dev/null 2>/dev/null")
+    warn "#{file} assembly doesn't compile" unless system("cc #{file}.s ./library.o -o #{file}.out")
+  end
 end
