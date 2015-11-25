1e68cf97479695cf8a0ebbe42d29d44a98131bf2
Shuyang Li <shuyang.li.95@gmail.com>
2015-11-24 21:02:22 -0500

    Implement symbol_code

--- symbol.c
+++ symbol.c
@@ -1,6 +1,7 @@
 #include <stdlib.h> // malloc
 #include <string.h> // memset
 #include "symbol.h"
+#include <math.h>   // log10, ceil
 
 struct symbol *symbol_create(symbol_t kind, int which, struct type *type, char *name) {
     struct symbol *s = (struct symbol *)malloc(sizeof(*s));
@@ -16,5 +17,32 @@ struct symbol *symbol_create(symbol_t kind, int which, struct type *type, char *
 }
 
 char *symbol_code(struct symbol *s) {
+    // for variables only, emits code that refers to the symbol
+    if (!s) {
+        fprintf(stderr, "calling symbol_code with NULL symbol\n");
+        exit(1);
+    }
 
+    switch (s->kind) {
+        case SYMBOL_GLOBAL: {
+            // globals are referred to by label
+            return s->name;
+        }
+        case SYMBOL_LOCAL: {
+            // locals are accessed with offset(%rbp)
+            int offset = 8 + s->param_count * 8 + s->which * 8;
+            int num_len = (int)ceil(log10(offset));
+            char *str = (char *)malloc(num_len + 8);
+            sprintf(str, "-%d(%%rbp)", offset);
+            return str;
+        }
+        case SYMBOL_PARAM: {
+            // params are accessed with offset(%rbp) too
+            int offset = 8 + s->which * 8;
+            int num_len = (int)ceil(log10(offset));
+            char *str = (char *)malloc(num_len + 8);
+            sprintf(str, "-%d(%%rbp)", offset);
+            return str;
+        }
+    }
 }
